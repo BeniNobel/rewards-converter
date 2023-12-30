eec3e0750d731d1c986d9b965115d7226ca3fcf3
BeniNobel <ibeninobel05@gmail.com>
2023-12-29 23:07:15 -0600

    created RewardValue updated RewardValueTests

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,27 @@
+public class RewardValue {
+
+    private final double cashValue;
+    private final int milesValue;
+
+    // Constructor accepting a cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = (int) (cashValue / 0.0035);
+    }
+
+    // Constructor accepting a miles value
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * 0.0035;
+    }
+
+    // Getter for cashValue
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Getter for milesValue
+    public int getMilesValue() {
+        return milesValue;
+    }
+}
